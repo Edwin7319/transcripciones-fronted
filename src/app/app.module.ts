a932c2cc7a36c05ae5f8fa527e1e8ed31bee2257
Edwin <edwin.paul.73@gmail.com>
2023-11-22 23:55:38 -0500

    feature: configure global tools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ToastrModule } from 'ngx-toastr';
+import { BreadcrumbModule } from 'xng-breadcrumb';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, BreadcrumbModule, ToastrModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
