008751b82a3c7fb02b54ac1fb1b330b8c02eaece
Edwin <edwin.paul.73@gmail.com>
2024-01-22 23:21:24 -0500

    feat: update labels

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -32,7 +32,7 @@ export class SidebarComponent implements OnInit {
           routerLink: APP_ROUTES.user,
         },
         {
-          label: 'Manejo de estados de audio',
+          label: 'Carga de transcripciones',
           routerLink: APP_ROUTES.handlingAudioRecording,
         },
       ],
